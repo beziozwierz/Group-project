9f9b5d941e90be7cf81909f34300a5c66a378b83
beziozwierz <kojot137@gmail.com>
2018-11-22 00:10:34 +0100

    added template of style editor: floats, changing size, TODO: fix issue with parent events

--- dragdrop/script.js
+++ dragdrop/script.js
@@ -1,13 +1,14 @@
 class Div {
     constructor(height, width, parent) {
-      this.inner = [];//new Array();
+      this.inner = [];
       this.parent = parent;
       this.height = height;
       this.width = width;
+      this.float = null;
     }
 }
 var dragged = new Div(100,100,null);
-
+var pathToEdited = null;
 /*model = new Div(600,400,null);
   model.inner[0] = new Div(400,400,model);
     model.inner[0].inner[0] = new Div(150,100,model.inner[0]);
@@ -20,47 +21,41 @@ model = new Div(800,600,null);
 
 
   function draw() {
-    var code = "";
-    var id = "model";
-    /*if(typeof model.inner == 'undefined'){return ""; }*/
-
-    /*
-    for(var i = 0;  i < model.inner.length ; i++){
-        code+='<div id="'+id+'_'+i+'" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="leave(event)"' +
-            ' style="border: 2px solid black; width: '+model.inner[i].width+'px; height: '+model.inner[i].height+'px;">\n';
-        code+=getInnercode(model.inner[i] , 1 , id+'_'+i );
-        code+='</div>\n';
-    }*/
-    code = getInnercode(model,0,"model");
+    var code = getInnercode(model,0,"model");
     var elem = document.getElementById("model");
     elem.innerHTML =  code;
-    console.log(code);
+    //console.log(code);
   }
 
   function getInnercode(div, depth, id){
-  /*  if(typeof div.inner == 'undefined' || div.inner.length == 0){
-      return "";
-    }
-    else{*/
-      var code = "";
-      for(var i = 0; i < div.inner.length ; i++){
+    var code = "";
 
-        for(var j=0;j<depth;j++){
-          code+="\t";
-        }
-        code+='<div id="'+id+'_'+i+'" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="leave(event)"' +
-            ' style="border: 2px solid black; '+
-            ' width: '+div.inner[i].width+'px; height: '+div.inner[i].height+'px;">\n';
+    var clear = false;
+    for(var i = 0; i < div.inner.length ; i++){
+        for(var j=0;j<depth;j++){ code+="\t"; }
+        code += '<div id="'+id+'_'+i+'"'+
+                ' ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="leave(event)" onclick="editStyle(this)"' +
+                ' style="border: 2px solid black; box-sizing: border-box; cursor: pointer;'+
+                ' width: ' + div.inner[i].width+'px;'+
+                ' height: ' + div.inner[i].height+'px;';
+                if( div.inner[i].float != null){
+                  clear = true;
+                  code += ' float: ' + div.inner[i].float +'">\n';
+                }else{
+                  if(clear == true){
+                    code += 'clear: both; ">';
+                    clear = false;
+                  }else{
+                    code += '">';
+                  }
+                }
 
-            code+=getInnercode(div.inner[i],depth+1,id+'_'+i);
+        code+=getInnercode(div.inner[i],depth+1,id+'_'+i);
 
-        for(var j=0;j<depth;j++){
-          code+="\t";
-        }
+        for(var j=0;j<depth;j++){ code+="\t"; }
         code+='</div>\n';
       }
       return code;
-    //}
   }
 
   function translateName(id){
@@ -74,27 +69,29 @@ model = new Div(800,600,null);
   function allowDrop(ev) {
       ev.preventDefault();
 
-
-      <!-- highlight -->
-/*
-      ev.target.addEventListener('mousedown', function() { this.parentNode.setAttribute("ondragover", "leave(event)"); });
-      ev.target.addEventListener('mouseup', function() { this.parentNode.setAttribute("ondragover", "allowDrop()"); });
-*/
-
+      /* highlight */
       var rect = ev.target.getBoundingClientRect();
       var condition = (ev.clientY - rect.top)/(rect.bottom - rect.top);
-      if(condition<0.2){ //top of div drop
-        ev.target.style.backgroundImage = "linear-gradient(black, white)";
-      }else if(condition<0.8){ //middle of div drop
-
-        ev.target.style.backgroundImage = "radial-gradient(black, white)";
-      }else{
-        ev.target.style.backgroundImage = "linear-gradient(white, black)";
+      if(condition<0.2){ //over top of div
+        ev.target.style.borderTop = "20px double green";
+        ev.target.style.borderBottom = "1px solid black";
+        ev.target.style.backgroundImage = "";
+        //ev.target.style.backgroundImage = "linear-gradient(black, white)";
+      }else if(condition<0.8){ //over middle of div
+        ev.target.style.borderTop = "1px solid black";
+        ev.target.style.borderBottom = "1px solid black";
+        ev.target.style.backgroundImage = "radial-gradient(green, lightgrey, lightgrey)";
+      }else{ //over bottom of div
+        ev.target.style.borderTop = "1px solid black";
+        ev.target.style.borderBottom = "20px double green";
+        ev.target.style.backgroundImage = "";
+        //ev.target.style.backgroundImage = "linear-gradient(white, black)";
       }
 
     }
-  function leave(event) {
+  function leave(event) { //reset div style (no highlight)
     event.target.style.backgroundImage = "";
+    event.target.style.border = "1px solid black";
   }
 
   function drop(ev) {
@@ -111,22 +108,56 @@ model = new Div(800,600,null);
 
           var rect = ev.target.getBoundingClientRect();
           var condition = (ev.clientY - rect.top)/(rect.bottom - rect.top); //position relative to parent
-          console.log("condition: "+condition);
 
           if(condition<0.2){ //top of div drop
             div.parent.inner.splice(index,0,new Div(dragged.height, dragged.width, div.parent));
-            console.log("cond1")
           }else if(condition<0.8){ //middle of div drop
-            if(typeof div.inner == 'undefined'){
-              div.inner[0] = new Div(dragged.height, dragged.width, div);
-              console.console.log("conde");
-            }else {
-              div.inner[div.inner.length] = new Div(dragged.height, dragged.width, div);
-              console.log("second and next");
-            }
-
-          }else if(condition <= 1){ //dla zwykłego else cond = infinity dla rodzica i rysuje
+            div.inner[div.inner.length] = new Div(dragged.height, dragged.width, div);
+          }else if(condition <= 1){ // <=1 (not else) important to not trigger parent (cond = inf for parent)
             div.parent.inner.splice(index+1,0,new Div(dragged.height, dragged.width, div.parent));
           }
             draw();
         }
+
+    function editStyle(element){  //TODO: parent is affected when clicked on nested div
+
+      var div = model;
+      var path = translateName(element.id);
+      for(var i = 0 ; i < path.length ; i++){
+          div = div.inner[path[i]];
+      }
+
+      pathToEdited = path;
+
+      code = '<b>Edytor styli</b>' +
+        '<br/>' +
+        'Width: <br/>' +
+        '<input type="text" id="editW" value="' + div.width + '"/> <br/>' +
+        'Height: <br/>' +
+        '<input type="text" id="editH" value="' + div.height + '"/> <br/>' +
+        'Float: <br/>' +
+        '<input type="text" id="editFloat" value="' + div.float + '"/> <br/>' +
+        '<button onclick="saveChanges()"> Save </button>';
+
+        elem = document.getElementById("styler");
+        elem.innerHTML = code;
+    }
+    function saveChanges(){
+      var w = document.getElementById("editW").value;
+      var h = document.getElementById("editH").value;
+      var f = document.getElementById("editFloat").value;
+
+      var div = model;
+      for(var i = 0 ; i < pathToEdited.length ; i++){
+          div = div.inner[pathToEdited[i]];
+      }
+      div.width = w;
+      div.height = h;
+      div.float = f;
+
+      draw();
+      //console.log(w);
+      // console.log(h);
+      // console.log(id);
+      //TODO: walidacja danych
+    }
