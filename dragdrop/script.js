7fc8dc329a6cc661200dd3f508f54bc9aaede5b6
beziozwierz <kojot137@gmail.com>
2018-11-21 16:35:38 +0100

    Fixed issues with inheritance (highlighting and dropping was affecting parent div)

--- dragdrop/script.js
+++ dragdrop/script.js
@@ -1,40 +1,47 @@
 class Div {
     constructor(height, width, parent) {
-      this.inner = new Array();
+      this.inner = [];//new Array();
       this.parent = parent;
       this.height = height;
       this.width = width;
-      this.rows = null;
     }
 }
 var dragged = new Div(100,100,null);
-model = new Div(600,400,null);
-model.inner[0] = new Div(200,100,model);
-model.inner[1] = new Div(150,150,model);
 
+/*model = new Div(600,400,null);
+  model.inner[0] = new Div(400,400,model);
+    model.inner[0].inner[0] = new Div(150,100,model.inner[0]);
+      model.inner[0].inner[0].inner[0] = new Div(50,50,model.inner[0].inner[0]);
+    model.inner[0].inner[1] = new Div(100,150,model.inner[0]);
+model.inner[1] = new Div(200,200,model);
+*/
+model = new Div(800,600,null);
+  model.inner[0] = new Div(800,600,model);
 
 
   function draw() {
     var code = "";
     var id = "model";
-    if(typeof model.inner == 'undefined'){return ""; }
+    /*if(typeof model.inner == 'undefined'){return ""; }*/
 
+    /*
     for(var i = 0;  i < model.inner.length ; i++){
         code+='<div id="'+id+'_'+i+'" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="leave(event)"' +
             ' style="border: 2px solid black; width: '+model.inner[i].width+'px; height: '+model.inner[i].height+'px;">\n';
         code+=getInnercode(model.inner[i] , 1 , id+'_'+i );
         code+='</div>\n';
-    }
+    }*/
+    code = getInnercode(model,0,"model");
     var elem = document.getElementById("model");
     elem.innerHTML =  code;
     console.log(code);
   }
 
   function getInnercode(div, depth, id){
-    if(typeof div.inner == 'undefined' || div.inner.length == 0){
+  /*  if(typeof div.inner == 'undefined' || div.inner.length == 0){
       return "";
     }
-    else{
+    else{*/
       var code = "";
       for(var i = 0; i < div.inner.length ; i++){
 
@@ -45,12 +52,7 @@ model.inner[1] = new Div(150,150,model);
             ' style="border: 2px solid black; '+
             ' width: '+div.inner[i].width+'px; height: '+div.inner[i].height+'px;">\n';
 
-
-        if(typeof div.inner[i].inner !== 'undefined'){
-          for(var i = 0 ; i < div.inner.length ; i++){
             code+=getInnercode(div.inner[i],depth+1,id+'_'+i);
-          }
-        }
 
         for(var j=0;j<depth;j++){
           code+="\t";
@@ -58,7 +60,7 @@ model.inner[1] = new Div(150,150,model);
         code+='</div>\n';
       }
       return code;
-    }
+    //}
   }
 
   function translateName(id){
@@ -74,19 +76,20 @@ model.inner[1] = new Div(150,150,model);
 
 
       <!-- highlight -->
-      ev.currentTarget.addEventListener('mousedown', function() { this.parentNode.setAttribute("ondragover", "leave(event)"); });
-      ev.currentTarget.addEventListener('mouseup', function() { this.parentNode.setAttribute("ondragover", "allowDrop()"); });
-
+/*
+      ev.target.addEventListener('mousedown', function() { this.parentNode.setAttribute("ondragover", "leave(event)"); });
+      ev.target.addEventListener('mouseup', function() { this.parentNode.setAttribute("ondragover", "allowDrop()"); });
+*/
 
       var rect = ev.target.getBoundingClientRect();
       var condition = (ev.clientY - rect.top)/(rect.bottom - rect.top);
       if(condition<0.2){ //top of div drop
-        ev.currentTarget.style.backgroundImage = "linear-gradient(black, white)";
+        ev.target.style.backgroundImage = "linear-gradient(black, white)";
       }else if(condition<0.8){ //middle of div drop
 
-        ev.currentTarget.style.backgroundImage = "linear-gradient(white, black, white)";
+        ev.target.style.backgroundImage = "radial-gradient(black, white)";
       }else{
-        ev.currentTarget.style.backgroundImage = "linear-gradient(white, black)";
+        ev.target.style.backgroundImage = "linear-gradient(white, black)";
       }
 
     }
@@ -112,15 +115,17 @@ model.inner[1] = new Div(150,150,model);
 
           if(condition<0.2){ //top of div drop
             div.parent.inner.splice(index,0,new Div(dragged.height, dragged.width, div.parent));
+            console.log("cond1")
           }else if(condition<0.8){ //middle of div drop
             if(typeof div.inner == 'undefined'){
               div.inner[0] = new Div(dragged.height, dragged.width, div);
+              console.console.log("conde");
             }else {
               div.inner[div.inner.length] = new Div(dragged.height, dragged.width, div);
               console.log("second and next");
             }
 
-          }else{
+          }else if(condition <= 1){ //dla zwykłego else cond = infinity dla rodzica i rysuje
             div.parent.inner.splice(index+1,0,new Div(dragged.height, dragged.width, div.parent));
           }
             draw();
