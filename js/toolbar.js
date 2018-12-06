9f2e1b8053f493e46f9e11c69ddf7909607bc225
beziozwierz <kojot137@gmail.com>
2018-12-06 03:17:18 +0100

    temporary merge

--- js/toolbar.js
+++ js/toolbar.js
@@ -2,40 +2,47 @@ function toolbar_element(option){
 
     switch(option.innerText) {
         case "header":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "title":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "settings":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "section":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "heade1r":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "navigation":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "article":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "footer":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "div":
-
+        document.getElementById("edit-element-title-container").innerHTML = ""+
+          '<div id="creator">'+
+          'Width: <br/>'+
+          '<input type="text" id="drag_width" /> <br/>'+
+          'Height: <br/>'+
+          '<input type="text" id="drag_height" /> <br/>'+
+          '<button onclick="createDiv()"> Create </button>'+
+          '</div>';
             break;
         case "list":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
         case "line":
-
+            document.getElementById("edit-element-title-container").innerHTML = "";
             break;
     }
 
     document.getElementById("edit-element-container").style.display = "block";
-    document.getElementById("edit-element-title-container").innerText = option.innerText;
+    //document.getElementById("edit-element-title-container").innerText = option.innerText;
 }
