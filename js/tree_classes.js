20ccb5750b333ee40d83e61b5e24dbff5f6b5c32
beziozwierz <kojot137@gmail.com>
2019-01-09 20:50:22 +0100

    added first version of zooming

--- js/tree_classes.js
+++ js/tree_classes.js
@@ -13,5 +13,11 @@ var global_name = "HTML";
 
 var dragged = new Div(global_name, '100','50px',null);
 var pathToEdited = null;
+
 model = new Div('root', 0,0,null);
 model.inner[0] = new Div('MainModel', '100','100%',model);
+
+ //Zmienne przechowujące obecny punkt odniesienia do rysowania
+var viewpoint_root = model;
+var viewpoint_name = "model";
+var zoomed = false;
