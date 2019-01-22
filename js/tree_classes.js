bb4b665f9a12c7e8b8cd1ccfd002e64832e06fcb
beziozwierz <kojot137@gmail.com>
2019-01-22 17:05:02 +0100

    Bugs fixed, refacored

--- js/tree_classes.js
+++ js/tree_classes.js
@@ -52,18 +52,3 @@ class Div {
     }
 }
 
-
-
-var global_name = "HTML";
-var global_type = "HTML";
-
-var dragged = new Div(global_name, '100','50px',null);
-var pathToEdited = null;
-
-model = new Div('root', 0,0,null);
-model.inner[0] = new Div('MainModel', '100','100%',model);
-
- //Zmienne przechowujące obecny punkt odniesienia do rysowania
-var viewpoint_root = model;
-var viewpoint_name = "model";
-var zoomed = false;
