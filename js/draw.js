2a3b48a0438afa5e066fe526679fba5f87910d94
beziozwierz <kojot137@gmail.com>
2019-01-23 16:19:30 +0100

    drag i drop elementow html dodany do index2

--- js/draw.js
+++ js/draw.js
@@ -11,7 +11,7 @@ function draw() {
   //code+=getModelDivCode(viewpoint_root,viewpoint_name);
    code += getModelDivCode(viewpoint_root,viewpoint_name);
    //console.log(code);
-   var elem = document.getElementById("model");
+   var elem = document.getElementById("model-display-container");
    elem.innerHTML =  code;
 }
 
---
+++ js/name_to_tag.js
@@ -0,0 +1,101 @@
+function nameToTag(text){
+  var name;
+  switch (text) {
+    case "Default container":
+      name = "div";
+      break;
+    case "Text container":
+      name = "span";
+      break;
+    case "Website container":
+      name = "iframe";
+      break;
+    case "Great header":
+      name = "h1";
+      break;
+    case "Huge header":
+      name = "h2";
+      break;
+    case "Big header":
+      name = "h3";
+      break;
+    case "Medium header":
+      name = "h4";
+      break;
+    case "Small header":
+      name = "h5";
+      break;
+    case "Tiny header":
+      name = "h6";
+      break;
+    case "Table":
+      name = "table";
+      break;
+    case "Table row":
+      name = "tr";
+      break;
+    case "Table cell":
+      name = "td";
+      break;
+    case "Ordered list":
+      name = "ol";
+      break;
+    case "Unordered list":
+      name = "ul";
+      break;
+    case "List element":
+      name = "li";
+      break;
+    case "Webpage link":
+      name = "a";
+      break;
+    case "Graphics":
+      name = "img";
+      break;
+    case "Audio":
+      name = "audio";
+      break;
+    case "Video":
+      name = "video";
+      break;
+    case "Form":
+      name = "form";
+      break;
+    case "Input":
+      name = "input";
+      break;
+    case "Textarea":
+      name = "textarea";
+      break;
+    case "Button":
+      name = "button";
+      break;
+    case "Select":
+      name = "select";
+      break;
+    case "Option":
+      name = "option";
+      break;
+    case "Header":
+      name = "header";
+      break;
+    case "Navigation":
+      name = "nav";
+      break;
+    case "Section":
+      name = "section";
+      break;
+    case "Article":
+      name = "article";
+      break;
+    case "Aside":
+      name = "aside";
+      break;
+    case "Footer":
+      name = "footer";
+      break;
+    default:
+      name="";
+  }
+  return name;
+}
